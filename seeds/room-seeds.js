1839250523cf4227cd195efed414b45cf6a3759b
Andrew Ahler <andrewahler@gmail.com>
2021-01-28 13:45:37 -0600

    added seeds

---
+++ seeds/room-seeds.js
@@ -0,0 +1,30 @@
+const { Room } = require("../models");
+
+const roomdata = [
+  {
+    Room: "1",
+    Equipment: "test 1",
+    Visitor: "test 1"
+  },
+
+  {
+    Room: "2",
+    Equipment: "test 2",
+    Visitor: "test 2"
+  },
+
+  {
+    Room: "3",
+    Equipment: "test 3",
+    Visitor: "test 3"
+  },
+
+  {
+    Room: "4",
+    Equipment: "test 4",
+    Visitor: "test 4"
+  },
+];
+const seedRoom = () => Room.bulkCreate(roomdata);
+
+module.exports = seedRoom;
