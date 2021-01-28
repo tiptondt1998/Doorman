ded9a59f98d962753cc88f372cfdc1a35338e02e
Andrew Ahler <andrewahler@gmail.com>
2021-01-28 13:32:19 -0600

    added seeds

---
+++ seeds/patient-seeds.js
@@ -0,0 +1,26 @@
+const { Patient } = require("../models");
+
+const patientdata = [
+  {
+    Name: "test 1",
+    Diagnosis: "test 1",
+  },
+
+  {
+    Name: "test 2",
+    Diagnosis: "test2",
+  },
+
+  {
+    Name: "test 3",
+    Diagnosis: "test 3",
+  },
+
+  {
+    Name: "test 4",
+    Diagnosis: "test 4",
+  },
+];
+const seedPatient = () => Nurse.bulkCreate(patientdata);
+
+module.exports = seedPatient;
