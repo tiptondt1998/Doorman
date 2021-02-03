671face90fdddc6825449455176b787126861546
jelly octane <jamie.de.lange@mac.com>
2021-02-03 17:27:58 -0600

    edits

--- models/room.js
+++ models/room.js
@@ -4,28 +4,24 @@ const { Model, DataTypes } = require('sequelize');
 class Room extends Model {}
 Room.init(
   {
-    id: {
+    /* id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
-    },
+    }, */
     roomNumber: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
-    // room_id: {
-    //   type: DataTypes.NUMBER,
-    //   allowNull: false
-    // }
+    /*  room_id: {
+       type: DataTypes.NUMBER,
+       allowNull: false
+    } */
   },
   {
     sequelize
   }
 );
-<<<<<<< HEAD
-module.exports = Room;
-=======
 module.exports = Room;
 
->>>>>>> 624d982e58e1fc9d8e1b3c7a2c5332d7466ca85f
