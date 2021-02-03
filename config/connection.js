cf5c21e54edd0db403a4c7f56b8cbe6e9721107e
jelly octane <jamie.de.lange@mac.com>
2021-02-03 14:05:07 -0600

    edits

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,22 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = process.env.JAWSDB_URL
+/* const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3001
+    },() => {
+      console.log("Database conection")
+    });
+
+module.exports = sequelize; */
+
+
+const sequelize = 
+  
+  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3001
