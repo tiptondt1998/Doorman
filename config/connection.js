bce0186cf970fd82f914fc2f2d93550f8320336a
jelly octane <jamie.de.lange@mac.com>
2021-02-14 12:45:12 -0600

    db fix

--- config/connection.js
+++ config/connection.js
@@ -17,7 +17,7 @@ if(process.env.JAWSDB_URL) {
 } */
 
 
-const sequelize = process.env.NODE_ENV === 'development' //heroku
+const sequelize = process.env.NODE_ENV === 'production' //heroku
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
